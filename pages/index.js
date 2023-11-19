539e3fce89445b7759048aee0b406289ac266960
gusde <69630915+bagussuprapta@users.noreply.github.com>
2023-11-19 16:34:09 +0800

    feat(player): filter players by position

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import { Inter } from "next/font/google";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Fragment } from "react";
+import { Listbox } from "@headlessui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const positions = ["GoalKeeper", "Defender", "Midfielder", "Forward"];
+
 export default function Home() {
   const [players, setPlayers] = useState([]);
+  const [selectedPosition, setSelectedPosition] = useState(positions[0]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:3000/api/players");
+        const response = await fetch(`http://localhost:3000/api/players?position=${selectedPosition}`);
         const result = await response.json();
         setPlayers(result);
       } catch (error) {
@@ -18,12 +22,27 @@ export default function Home() {
     };
 
     fetchData();
-  }, []);
+  }, [selectedPosition]);
 
   return (
     <main className={`${inter.className} px-8 py-2`}>
       <h1 className="mb-3">Footbal Player List</h1>
-      <div className="grid grid-cols-3 gap-2">
+      <Listbox value={selectedPosition} onChange={setSelectedPosition}>
+        <Listbox.Button className={"bg-slate-500 text-white px-2 py-1 rounded"}>{selectedPosition}</Listbox.Button>
+        <Listbox.Options className={"mt-2"}>
+          {positions.map((position, index) => (
+            <Listbox.Option key={index} value={position} as={Fragment}>
+              {({ active, selected }) => (
+                <li className={`${active ? "bg-blue-500 text-white py-1 px-2 rounded cursor-pointer" : "bg-white text-black py-1 px-2"}`}>
+                  {selected}
+                  {position}
+                </li>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </Listbox>
+      <div className="grid grid-cols-3 gap-2 mt-2">
         {players.length > 0 ? (
           players.map((player, index) => {
             return (
